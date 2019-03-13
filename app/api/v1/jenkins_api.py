79eba40d4706e24dd1ce79e9b4a1ab290efba326
zhangtingli <zhangtingli@kuainiugroup.com>
2019-03-13 20:06:09 +0800

    调整

--- app/api/v1/jenkins_api.py
+++ app/api/v1/jenkins_api.py
@@ -17,24 +17,27 @@ from app.bussinse.CaseBiz import CaseBiz
 
 @api_v1.route("/run/case", methods=['POST'])
 def run_special_case():
-    case_biz = CaseBiz()
-    jenkins_url = "https://jenkins-test.kuainiujinke.com/jenkins/"
-    user_id = "zhangtingli"
-    user_pwd = "123456"
-    case_ids = request.json['case_ids']
-    confirm_cases=[]
-    email = request.json['email']
-    print(case_ids)
-    for case in case_ids:
-        if case_biz.check_execstatus_bycaseid(case)==False:
-            return jsonify(CommonResult.fill_result(case,1,"case_id:{0} 执行状态不正确不能被执行".format(case)))
-    exec_case_array = case_biz.get_exec_caseid(case_ids)
-    print(exec_case_array)
-    exec_case_array_str = ','.join(str(case) for case in exec_case_array)
-    server = jenkins.Jenkins(jenkins_url,user_id,user_pwd)
+    try:
+        case_biz = CaseBiz()
+        jenkins_url = "https://jenkins-test.kuainiujinke.com/jenkins/"
+        user_id = "zhangtingli"
+        user_pwd = "123456"
+        case_ids = request.json['case_ids']
+        confirm_cases=[]
+        email = request.json['email']
+        print(case_ids)
+        for case in case_ids:
+            if case_biz.check_execstatus_bycaseid(case)==False:
+                return jsonify(CommonResult.fill_result(case,1,"case_id:{0} 执行状态不正确不能被执行".format(case)))
+        exec_case_array = case_biz.get_exec_caseid(case_ids)
+        exec_case_array_str = ','.join(str(case) for case in exec_case_array)
+        server = jenkins.Jenkins(jenkins_url,user_id,user_pwd)
 
-    build_number = server.build_job("Auto_Test_Api_Run_Case",parameters={"case_ids":exec_case_array_str,"email_address":email})
-    return jsonify(CommonResult.fill_result(build_number))
+        build_number = server.build_job("Auto_Test_Api_Run_Case",parameters={"case_ids":exec_case_array_str,"email_address":email})
+        return jsonify(CommonResult.fill_result(build_number))
+    except Exception as e:
+        jsonify(CommonResult.fill_result(build_number,1,"jenkins 构建失败"))
+
 
 
 
