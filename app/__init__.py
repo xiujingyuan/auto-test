809351165c2ca1d52f9c972645127e6e64ae8ad2
snow <fengxuantianya@163.com>
2019-07-25 19:30:27 +0800

    配置celery和redis

--- app/__init__.py
+++ app/__init__.py
@@ -12,18 +12,29 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from app.common.config.config import config
 from flask_wtf.csrf import CSRFProtect
+from celery import Celery
+from environment.common.config import Config
+from redis import Redis
 
 db = SQLAlchemy()
 csrf = CSRFProtect()
+
+celery = Celery(__name__,
+                broker=Config.CELERY_BROKER_URL,
+                backend=Config.CELERY_RESULT_BACKEND)
 
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
-    config[config_name].init_app(app)
 
+    celery.conf.update(app.config)
+    app.app_redis = Redis(host=app.config['REDIS_HOST'],
+                          port=app.config['REDIS_PORT'],
+                          db=5,
+                          password=app.config['REDIS_PWD'])
     #csrf.init_app(app)
-
+    config[config_name].init_app(app)
     db.init_app(app)
     if not app.debug and not app.testing and not app.config['SSL_DISABLE']:
         from flask_sslify import SSLify
