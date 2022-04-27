b81238533fe767eb15935a98c59c4914fef99dea
fengxuantianya <fengxuantianya@163.com>
2022-04-27 17:46:58 +0800

    修改放款

--- app/services/grant.py
+++ app/services/grant.py
@@ -416,9 +416,10 @@ class OverseaGrantService(GrantBaseService):
                 asset_import_sync_task = self.db_session.query(Synctask).filter(
                     Synctask.synctask_order_no == task.sendmsg_order_no,
                     Synctask.synctask_type.in_(('BCAssetImport', 'DSQAssetImport'))).first()
-                item_no = asset_import_sync_task.synctask_order_no if \
-                    channel == 'noloan' else asset_import_sync_task.synctask_order_no.replace(channel, '')
-                return json.loads(asset_import_sync_task.synctask_request_data), item_no
+                if asset_import_sync_task is not None:
+                    item_no = asset_import_sync_task.synctask_order_no if \
+                        channel == 'noloan' else asset_import_sync_task.synctask_order_no.replace(channel, '')
+                    return json.loads(asset_import_sync_task.synctask_request_data), item_no
         LogUtil.log_info('not fount the asset import task')
         raise ValueError('not fount the asset import task')
 
