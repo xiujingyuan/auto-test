6eb6223574f65369648de2b3da09c855506cc850
fengxuantianya <fengxuantianya@163.com>
2022-10-27 18:01:55 +0800

    添加放款

--- app/services/capital_service/beiyin_daqin.py
+++ app/services/capital_service/beiyin_daqin.py
@@ -4,5 +4,10 @@ from app.services.capital_service.jinmeixin_daqin import JinmeixindaqinMock
 class BeiyindaqinMock(JinmeixindaqinMock):
 
     def __init__(self, project, asset, asset_extend, asset_tran_list, period_start, period_end):
-        self.channel = 'beiyin_daqin'
         super(BeiyindaqinMock, self).__init__(project, asset, asset_extend, asset_tran_list, period_start, period_end)
+        self.channel = 'beiyin_daqin'
+        self.trail_url = '/chongtian/{0}/repay/calc'.format(self.channel)
+        self.trail_query_url = ''
+        self.repay_plan_url = ''
+        self.repay_apply_url = '/chongtian/{0}/repay/request'.format(self.channel)
+        self.repay_apply_query_url = '/chongtian/{0}/repay/queryStatus'.format(self.channel)
