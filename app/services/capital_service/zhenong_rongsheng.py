4fcd10bdccc7201e84c7725adc949f475b849049
fengxuantianya <fengxuantianya@163.com>
2022-12-05 18:42:44 +0800

    添加浙农的试算

--- app/services/capital_service/zhenong_rongsheng.py
+++ app/services/capital_service/zhenong_rongsheng.py
@@ -1,21 +1,21 @@
 from app.services.capital_service import BusinessMock
 
 
-class YixinrongshengMock(BusinessMock):
+class ZhenongrongshengMock(BusinessMock):
 
     def __init__(self, project, asset, asset_extend, asset_tran_list, period_start, period_end):
-        self.channel = 'yixin_rongsheng'
-        super(YixinrongshengMock, self).__init__(project, asset, asset_extend, asset_tran_list, period_start, period_end)
-        self.trail_url = '/yixin/yixin_rongsheng/calAllAmountInAdvanceSingle'
+        self.channel = 'zhenong_rongsheng'
+        super(ZhenongrongshengMock, self).__init__(project, asset, asset_extend, asset_tran_list, period_start, period_end)
+        self.trail_url = '/zhenong/zhenong_rongsheng/repay/query'
         self.trail_query_url = ''
         self.repay_plan_url = ''
         self.repay_apply_url = ''
-        self.repay_apply_query_url = '/yixin/yixin_rongsheng/repay.result'
+        self.repay_apply_query_url = '/zhenong/zhenong_rongsheng/repay/result/query'
 
     def repay_trail_mock(self, status, principal_over=False, interest_type='less'):
         principal_amount, interest_amount, _, _, repayPlanDict = self.__get_trail_amount__()
-        value = dict(zip(('$.data.lnsCurAmt', '$.data.lnsCurInt'),
-                         (repayPlanDict[min(repayPlanDict.keys())]['principal'] * 100, interest_amount)))
+        value = dict(zip(('$.data.data.capital', '$.data.data.interest'),
+                         (principal_amount, interest_amount)))
         return self.update_by_json_path(self.trail_url, value, method='post')
 
     def repay_apply_query_mock(self, withhold, withhold_detail, success_type='success'):
