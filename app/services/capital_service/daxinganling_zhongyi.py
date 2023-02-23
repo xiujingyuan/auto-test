8c132fdeec5e965f8a263ab1f1468fbe5d02349e
fengxuantianya <fengxuantianya@163.com>
2023-02-23 15:17:34 +0800

    添加大兴安岭的试算和查询

--- app/services/capital_service/daxinganling_zhongyi.py
+++ app/services/capital_service/daxinganling_zhongyi.py
@@ -37,6 +37,6 @@ class DaxinganlingzhongyiMock(BusinessMock):
         interest = interest_detail[0] if interest_detail else 0
         code = 2000 if success_type.lower() == 'success' else 30000
         value = dict(zip(('$.data[0].deductionSerialNo', '$.data[0].state', '$.data[0].principal',
-                          '$.data[0].interest', '$.data[0].responseCode'),
+                          '$.data[0].interest', '$.code'),
                          (withhold.withhold_serial_no, success_type, principal, interest, code)))
         return self.update_by_json_path(self.repay_apply_query_url, value, method='post')
