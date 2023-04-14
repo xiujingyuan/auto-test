10d8a0b260df0abe224a2ee04d848097eada3419
fengxuantianya <fengxuantianya@163.com>
2023-04-14 15:34:16 +0800

    调整长银查询code

--- app/services/china/biz_central/service.py
+++ app/services/china/biz_central/service.py
@@ -293,15 +293,14 @@ class ChinaBizCentralService(BaseService):
         task_list = self.db_session.query(CentralTask).filter(
             CentralTask.task_order_no.in_(task_order_no),
             CentralTask.task_update_at >= max_create_at)\
-            .order_by(desc(CentralTask.task_id)).paginate(page=1, per_page=15, error_out=False).items
+            .order_by(desc(CentralTask.task_id)).paginate(page=1, per_page=25, error_out=False).items
 
         other_task_list = self.db_session.query(CentralTask).filter(
             or_(
                 CentralTask.task_order_no.like('settle_detail_{0}%'.format(channel)),
                 CentralTask.task_order_no.like('REPAYMENTFILE_%'),
                 CentralTask.task_order_no.like('COMPFILE_%'),
-                CentralTask.task_order_no.like('{0}%'.format(channel)),
-                CentralTask.task_order_no.like('{0}%'.format(item_no))),
+                CentralTask.task_order_no.like('{0}%'.format(channel))),
             CentralTask.task_update_at >= self.get_date(is_str=True, fmt='%Y-%m-%d')) \
             .order_by(desc(CentralTask.task_id)).paginate(page=1, per_page=5, error_out=False).items
 
