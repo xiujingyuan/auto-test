6a0bf03c688eb53c0000e7feb6c1dc21e166905a
fengxuantianya <fengxuantianya@163.com>
2023-05-17 10:51:04 +0800

    添加哈银还款查询接口mock

--- app/services/capital_service/hayin_zhongbao.py
+++ app/services/capital_service/hayin_zhongbao.py
@@ -4,11 +4,11 @@ from functools import reduce
 from app.services.capital_service import BusinessMock
 
 
-class Hayin_zhongbaoMock(BusinessMock):
+class HayinzhongbaoMock(BusinessMock):
 
     def __init__(self, project, asset, asset_extend, asset_tran_list, period_start, period_end):
         self.channel = 'hayin_zhongbao'
-        super(Hayin_zhongbaoMock, self).__init__(project, asset, asset_extend, asset_tran_list, period_start,
+        super(HayinzhongbaoMock, self).__init__(project, asset, asset_extend, asset_tran_list, period_start,
                                                     period_end)
         self.trail_url = '/hayin/hayin_zhongbao/api/repay/query/trial'
         self.trail_query_url = ''
