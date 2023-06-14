1047d6cbc49a24623730c12fb424857f44793999
fengxuantianya <fengxuantianya@163.com>
2023-06-14 11:14:14 +0800

    添加唯品试算和查询

---
+++ app/services/capital_service/weipin_hanchen_jf.py
@@ -0,0 +1,45 @@
+from decimal import Decimal
+
+from app.services.capital_service.jinmeixin_daqin import JinmeixindaqinMock
+
+
+class WeipinhanchenjfMock(JinmeixindaqinMock):
+
+    def __init__(self, project, asset, asset_extend, asset_tran_list, period_start, period_end):
+        super(WeipinhanchenjfMock, self).__init__(project, asset, asset_extend, asset_tran_list,
+                                                     period_start, period_end)
+        self.channel = 'weipin_hanchen_jf'
+        self.trail_url = '/jingfa/weipin_hanchen_jf/kuainiu/apply/repayTrial'
+        self.trail_query_url = ''
+        self.repay_plan_url = ''
+        self.repay_apply_url = '/jingfa/weipin_hanchen_jf/kuainiu/apply/repayApply'
+        self.repay_apply_query_url = '/jingfa/weipin_hanchen_jf/kuainiu/query/repayResultQuery'
+
+    def repay_trail_mock(self, status, principal_over=False, interest_type='less'):
+        principal_amount, interest_amount, _, _, _ = self.__get_trail_amount__()
+        if interest_type == 'less':
+            interest_amount -= 100
+        elif interest_type == 'more':
+            interest_amount += 100
+        elif interest_type == "zero":
+            interest_amount = 0
+        value = dict(zip(('$.data.data.repaymentPrinciple', '$.data.data.repaymentInterest',
+                          '$.data.data.repaymentTotalAmt'), (
+                             principal_amount,
+                             interest_amount,
+                             interest_amount + principal_amount)))
+        return self.update_by_json_path(self.trail_url, value, method='post')
+
+    def repay_apply_query_mock(self, withhold, withhold_detail, success_type='PART'):
+        principal_amount, interest_amount, fee_amount, late_amount, _ = self.__get_trail_amount__()
+        code = '1' if success_type.lower() == 'success' else '2'
+        period = min(list(map(lambda x: x.withhold_detail_period, withhold_detail)))
+        value = dict(zip(('$.data.data.repayList[0].loanNo', '$.data.data.repayList[0].period',
+                          '$.data.data.repayList[0].repayDate', '$.data.data.repayList[0].repaymentInterest',
+                          '$.data.data.repayList[0].repaymentPrinciple',
+                          '$.data.data.repayList[0].repaymentTotalAmt',
+                          '$.data.data.repayList[0].status'),
+                          (self.asset.asset_due_bill_no, period, self.get_date(is_str=True, fmt='%Y%m%d'),
+                           interest_amount,
+                           principal_amount, withhold.withhold_amount, code)))
+        return self.update_by_json_path(self.repay_apply_query_url, value, method='post')
